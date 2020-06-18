e4d51103502e0c2e64337f3233785c9e1286ffd5
Shantanu Jamble <sjamble24@gmail.com>
2020-06-18 14:54:03 -0400

    commiting local changes for debug help

--- StormBreaker/StormBreaker/Engine/Application.cpp
+++ StormBreaker/StormBreaker/Engine/Application.cpp
@@ -125,13 +125,13 @@ namespace Engine {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
+		//layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* layer)
 	{
 		m_LayerStack.PushOverlay(layer);
-		layer->OnAttach();
+		//layer->OnAttach();
 	}
 
 
--- StormBreaker/StormBreaker/Engine/LayerStack.cpp
+++ StormBreaker/StormBreaker/Engine/LayerStack.cpp
@@ -7,14 +7,17 @@ namespace Engine
 	LayerStack::~LayerStack()
 	{
 		for (Layer* layer : m_Layers)
+		{
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
 	{
 		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
 		m_LayerInsertIndex++;
-		//layer->OnAttach();
+		layer->OnAttach();
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
